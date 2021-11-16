dc222b6eb15daf49cfb4f26981e9a0b75a9cabbb
prateekkoul89 <75244048+prateekkoul89@users.noreply.github.com>
2021-11-16 13:25:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,10 @@
 resource "aws_instance" "ec1" {
     for_each = toset(var.new)
-    ami = var.myami
+    ami = "ami-04ad2567c9e3d7893"
     instance_type = "t2.micro"
     
     tags =  {
-        Name = "vm${each.key}"
+        Name = "cloud"
     }
   
 }
-
